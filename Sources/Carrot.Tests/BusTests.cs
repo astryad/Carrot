8391a27448f82568e0622dd3ffe364a9de4df9b9
Thibault Falise <thibault.falise@gdfsueztrading.com>
2015-11-02 12:47:07 +0100

    Should_ensure_exchange_exists_when_publishing_a_text_message_to_a_specific_exchange

--- Sources/Carrot.Tests/BusTests.cs
+++ Sources/Carrot.Tests/BusTests.cs
@@ -1,20 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FakeItEasy;
 using NFluent;
 using NUnit.Framework;
+using RabbitMQ.Client;
 
 namespace Carrot.Tests
 {
     public class BusTests
     {
+        private IConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private IModel _model;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _connectionFactory = A.Fake<IConnectionFactory>();
+            _model = A.Fake<IModel>();
+            _connection = A.Fake<IConnection>();
+
+        }
+
         [Test]
         public void Should_connect_to_provided_host_when_a_new_bus_is_created()
         {
-            var bus = new Bus("host");
+            var bus = new Bus("host", _connectionFactory);
+
             Check.That(bus.Host).Equals("host");
+        }
+
+        [Test]
+        public void Should_create_a_new_connection_when_creating_a_new_bus()
+        {
+            A.CallTo(() => _connectionFactory.CreateConnection()).Returns(_connection);
+
+            var bus = new Bus("host", _connectionFactory);
+
+            A.CallTo(() => _connectionFactory.CreateConnection()).MustHaveHappened();
+        }
+
+        [Test]
+        public void Should_ensure_exchange_exists_when_publishing_a_text_message_to_a_specific_exchange()
+        {
+            A.CallTo(() => _connectionFactory.CreateConnection()).Returns(_connection);
+            A.CallTo(() => _connection.CreateModel()).Returns(_model);
+
+            var bus = new Bus("host", _connectionFactory);
+            bus.Publish("Hello world!");
+
+            A.CallTo(() => _model.ExchangeDeclare("amq.direct", "direct")).MustHaveHappened();
         }
     }
 }
--- Sources/Carrot/Bus.cs
+++ Sources/Carrot/Bus.cs
@@ -1,12 +1,28 @@
-﻿namespace Carrot
+﻿using RabbitMQ.Client;
+
+namespace Carrot
 {
     public class Bus
     {
+        private IConnection _connection;
+
         public Bus(string host)
+            : this(host, new ConnectionFactory())
+        {
+        }
+
+        public Bus(string host, IConnectionFactory connectionFactory)
         {
             Host = host;
+            _connection = connectionFactory.CreateConnection();
         }
 
         public string Host { get; }
+
+        public void Publish(string message)
+        {
+            var model = _connection.CreateModel();
+            model.ExchangeDeclare("amq.direct", "direct");
+        }
     }
 }
